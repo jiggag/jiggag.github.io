7a5cd6b13547852b7839fbe0b27c597f756c8900
jiggag <jiggag90@gmail.com>
2022-01-16 15:58:32 +0900

    404 페이지 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,52 @@
 import * as React from 'react';
 import { Layout } from 'components/Layout';
+import { graphql, Link, PageProps } from 'gatsby';
 import { Seo } from 'components/Seo';
+import { AllMarkdownRemarkProps } from 'types';
 
-const NotFoundPage = function () {
+const NotFoundPage = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}: PageProps<AllMarkdownRemarkProps<'title' | 'slug' | 'published' | 'date'>>) {
   return (
     <Layout>
       <Seo title="404: Not found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="not-found-title">
+        찾을 수 없는 페이지 입니다
+        <p>- 잘못된 주소이거나</p>
+        <p>- 해당 글이 사라졌나봐요</p>
+      </div>
+
+      <div className="not-found-title">
+        최근 등록된 페이지를 확인해보세요!
+        {edges.filter(({ node }) => node.frontmatter.published).splice(0, 10).map(({ node: { id, frontmatter: { title, slug, date } } }) => (
+          <Link to={slug} key={id}>
+            <p>- {title}</p>
+          </Link>
+        ))}
+      </div>
+
     </Layout>
   );
 };
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+    query {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        slug
+                        title
+                        published
+                        date(formatString: "YYYY-MM-DD")
+                    }
+                }
+            }
+        }
+    }
+`;
